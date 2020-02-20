099f1b3bc6640ccee98d1d23c5d523eefa1935ae
iriojose <iriojose>
2020-02-19 21:02:32 -0400

    fix next.config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 module.exports = {
-    env: {
-        customKey: 'my-value',
-    },
-    pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
+  env: {
+    customKey: "my-value"
+  },
 
-    /*Ejemplo para produccion, para subir en servidor apache (config de rutas).
-    exportPathMap: async function(
-        defaultPathMap,
-        { dev, dir, outDir, distDir, buildId }
-    )
-    {
-        return {
-          '/': { page: '/' },
-          '/about': { page: '/about' },
-        }
-    },*/
+  pageExtensions: ["mdx", "jsx", "js", "ts", "tsx"],
+
+  exportTrailingslash: true,
+
+  //Ejemplo para produccion, para subir en servidor apache (config de rutas).
+  exportPathMap: function() {
+    const paths = {
+      "/": { page: "/" },
+      "/login": { page: "/login" },
+      "/registro": { page: "/registro" },
+      "/about": { page: "/about" }
+    };
+
+    return paths;
+  }
 };
--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -1,44 +1,53 @@
-import React from 'react';
-import Router from 'next/router'
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Divider from '@material-ui/core/Divider';
+import React from "react";
+import Router from "next/router";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
 }));
 
 export default function ButtonAppBar() {
   const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <AppBar>
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Task Manager
-                    </Typography>
-                    <Button color="inherit" onClick={() => Router.push('/login')}>Login</Button>
-                    <Divider orientation="vertical" flexItem light />
-                    <Button color="inherit" onClick={() => Router.push('/registro')}>Registrate</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      <AppBar>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Task Manager
+          </Typography>
+          <Button color="inherit" onClick={() => Router.push("/login")}>
+            <Typography variant="body1">Iniciar sesión</Typography>
+          </Button>
+          <Divider orientation="vertical" flexItem light />
+          <Button variant="outlined" onClick={() => Router.push("/registro")}>
+            <Typography variant="body1">Registrarse</Typography>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
